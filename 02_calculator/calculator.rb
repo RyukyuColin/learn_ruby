a8c72156e140280f363da415987f3dfd06a99212
Colin Richardson <richardsonjcolin@gmail.com>
2017-04-04 15:31:17 +0900

    resolved failures

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
-#write your code here
+def add number1, number2
+
+	sum = number1 + number2
+
+end
+
+def subtract number1, number2
+
+	diff = number1 - number2
+
+end
+
+def sum array
+
+	s = 0
+
+	array.each do |n|
+		s += n
+	end
+
+	return s
+
+end
+	
