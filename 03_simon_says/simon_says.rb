23fc2d42668d57caa795c2f3039d1cdd410fbec4
Colin Richardson <richardsonjcolin@gmail.com>
2017-04-04 17:57:13 +0900

    resolved all errors until 'titleize'

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,50 @@
-#write your code here
+def echo response
+
+	response.to_s
+
+end
+
+def shout response
+
+	response.to_s.upcase
+	
+end
+
+def repeat response, n=2
+
+	((response + ' ') * n).chop 
+	
+end
+
+def start_of_word response, n
+
+	response.to_s[0.. n - 1]
+	
+end
+
+def first_word response
+
+	response.split.first
+
+end
+
+
+def titleize response
+	
+	words = response.split
+
+	words.each do |word|
+
+		if word == 'and' or word == 'over' or word == 'the'
+			word.downcase!
+		elsif word.length >= 3
+			word.capitalize!
+		end
+	end
+	
+	words.join ' '
+		
+end
+
+
+# response.gsub(/\w+/) {|word| word.capitalize}
