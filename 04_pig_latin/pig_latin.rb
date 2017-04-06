73671248dec2474aaa5715585fafdb808d3b08b0
Colin Richardson <richardsonjcolin@gmail.com>
2017-04-06 19:53:08 +0900

    resolved all errors

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,55 @@
-#write your code here
+def translate words
+
+	words = words.split
+
+	words.map! do |word|
+		if is_vowel(word[0])
+			word += "ay"		
+		elsif word[0] == "s" and word[1] == "q" and word[2] == "u"
+			word = word.split("")
+			temp = ""
+			temp = word.shift(3)
+			word = word.push(temp)
+			word = word.join + "ay"
+		elsif word[0] == "q" and word[1] == "u"
+			word = word.split("")
+			temp = ""
+			temp = word.shift(2)
+			word = word.push(temp)
+			word = word.join + "ay"				
+		elsif !is_vowel(word[0]) and !is_vowel(word[1]) and !is_vowel(word[2])
+			word = word.split("")
+			temp = ""
+			temp = word.shift(3)
+			word = word.push(temp)
+			word = word.join + "ay"
+		elsif !is_vowel(word[0]) and !is_vowel(word[1])			
+			word = word.split("")
+			temp = ""
+			temp = word.shift(2)
+			word = word.push(temp)
+			word = word.join + "ay"			
+		elsif  !is_vowel(word[0])
+			word = word.split("")
+			temp = ""
+			temp = word.shift
+			word = word.push(temp)
+			word = word.join + "ay"
+		end
+	end
+
+	words.join(" ")
+
+end
+
+
+
+def is_vowel letter
+	if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u"
+		true
+	else
+		false
+	end
+end		
+
+puts translate("hello world what are you doing")
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def title
+		title_array = @title.split
+		title_array.each do |word|
+			if ["and", "in", "the", "of", "a", "an"].index(word) == nil  
+				word.capitalize!				
+			end			
+		end
+		title_array[0].capitalize!
+		title_array.join(" ")
+	end
 end
