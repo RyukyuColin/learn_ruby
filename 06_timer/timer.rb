4fdddef2e6c80caa3e0d2839c87a2103f9b6506f
Colin Richardson <richardsonjcolin@gmail.com>
2017-04-07 19:43:15 +0900

    resolved all errors and extra rspec errors

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
-  #write your code here
+	attr_accessor :seconds
+
+	def initialize
+		@seconds = 0
+	end
+
+	def time_string
+		Time.at(@seconds).utc.strftime("%H:%M:%S")
+	end
+
+	def padded num
+		if num < 60
+			Time.at(num).utc.strftime("%S")	
+		end
+		
+	end
 end
+
